3d9a78374dc72dfc035142837b5bbb26a7357652
Jair Zea Paez <jairzeapaez@MacBook-Air-de-Jair.local>
2023-11-10 16:34:46 -0500

    animation

--- src/app/components/big-carousel/big-carousel.component.ts
+++ src/app/components/big-carousel/big-carousel.component.ts
@@ -4,7 +4,7 @@ import { DomSanitizer } from '@angular/platform-browser';
 @Component({
   selector: 'app-big-carousel',
   templateUrl: './big-carousel.component.html',
-  styleUrls: ['./big-carousel.component.css'],
+  styleUrls: ['./big-carousel.component.css', './big-carousel-animation.component.css'],
 })
 export class BigCarouselComponent {
 
@@ -16,6 +16,8 @@ export class BigCarouselComponent {
 
   logos: any = [];
   backgroundImageStyle: any;
+  selectedLogo: any;
+  animation: boolean = true
 
   showNavigationArrows = false;
   showNavigationIndicators = false;
@@ -37,50 +39,90 @@ export class BigCarouselComponent {
           'assets/images/components/carousel-images/asocebu2.jpg'
         ),
         name: 'Logo 1',
-        title: 'Texto del logo 1', showText: false 
+        title: 'Texto del logo 1', showText: false, selected: false 
       },
       {
         url: this.getSafeImageUrl(
           'assets/images/components/carousel-images/asosimental.jpg'
         ),
         name: 'Logo 2',
-        title: 'Texto del logo 2', showText: false 
+        title: 'Texto del logo 2', showText: false, selected: false 
       },
       {
         url: this.getSafeImageUrl(
           'assets/images/components/carousel-images/asovinos.jpg'
         ),
         name: 'Logo 3',
-        title: 'Texto del logo 3', showText: false 
+        title: 'Texto del logo 3', showText: false, selected: false 
       },
       {
         url: this.getSafeImageUrl(
           'assets/images/components/carousel-images/astegan.png'
         ),
         name: 'Logo 4',
-        title: 'Texto del logo 4', showText: false 
+        title: 'Texto del logo 4', showText: false, selected: false 
       },
       {
         url: this.getSafeImageUrl(
           'assets/images/components/carousel-images/decarne.png'
         ),
         name: 'Logo 5',
-        title: 'Texto del logo 5', showText: false 
+        title: 'Texto del logo 5', showText: false, selected: false 
       },
       {
         url: this.getSafeImageUrl(
           'assets/images/components/carousel-images/fng-blanco.jpg'
         ),
         name: 'Logo 6',
-        title: 'Texto del logo 6', showText: false 
+        title: 'Texto del logo 6', showText: false, selected: false 
       },
       {
         url: this.getSafeImageUrl(
           'assets/images/components/carousel-images/logo_agrousol.gif'
         ),
         name: 'Logo 7',
-        title: 'Texto del logo 7', showText: false 
+        title: 'Texto del logo 7', showText: false, selected: false 
       },
     ];
+  }
+  onImageClick(logo: any, index: number) {
+    console.log('Clic en la imagen:', logo);
+    // ocultar/mostrar contenido adicional
+    logo.showText = !logo.showText;
+    // Deseleccionar todas las imágenes
+    this.logos.forEach((l: any) => (l.selected = false));
+
+    // Seleccionar la imagen clicada
+    logo.selected = true;
+    this.selectedLogo = logo;
+    this.animation = false
+
+    // Mover la imagen clicada al centro del array
+    const currentIndex = this.logos.indexOf(logo);
+    const middleIndex = Math.floor(this.logos.length / 2);
+
+    const diff = middleIndex - currentIndex;
+
+    if (diff !== 0) {
+      this.logos = this.shiftArray(this.logos, diff);
+    }
+  }
+
+  shiftArray(arr: any[], diff: number): any[] {
+    const shiftedArray = [...arr];
+
+    for (let i = 0; i < Math.abs(diff); i++) {
+      if (diff > 0) {
+        // Shift right
+        const lastItem = shiftedArray.pop();
+        shiftedArray.unshift(lastItem);
+      } else {
+        // Shift left
+        const firstItem = shiftedArray.shift();
+        shiftedArray.push(firstItem);
+      }
+    }
+
+    return shiftedArray;
   }
 }
